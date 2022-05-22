bc80d3d4e1f36988d69d1c3cb76465555f0ccb2b
Terry Grimoire <terrt.grimoire@gmail.com>
2022-05-23 00:32:08 +0200

    changing small css details and name in nav bar to contact

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -9,7 +9,7 @@ function Nav() {
         <ul className='nav_ul'>
             <Link to="/" > <li> Main Stage </li> </Link>
             <Link to="/Queens" > <li> Queens </li> </Link>
-            <Link to="/About" > <li> About us </li> </Link>
+            <Link to="/About" > <li> Contact </li> </Link>
             <Link to="/SashayAway" > <li> Sashay Away </li> </Link>
         </ul>
       </section>
