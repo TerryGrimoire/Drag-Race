211df968dca86ac2218262d0081c9bda320f45df
Terry Grimoire <terrt.grimoire@gmail.com>
2022-05-23 00:01:22 +0200

    adding new style to queens

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Cards({queen}) {
 
+    const [mouse, setMouse] = useState(false);
+    const nameofClass = mouse? 'show': 'hide';
+
     return (
-        <div className='cards'>
-            <h2 className='cards-title'> {queen.name} </h2>
+        <div className='cards' onMouseOver={()=> setMouse(true)} onMouseLeave={()=> setMouse(false)} >
             <img src={queen.image_url} alt={queen.name} className='cards-image'   />
-            <div className='cards-description-container'>
+            <h3 className='cards-title'> {queen.name} </h3>
+            <div className={nameofClass}>
             {queen.seasons? <p>Season {queen.seasons[0].seasonNumber} </p>:''}
-            {queen.quote !== `""` ? <p> Favourite quote : {queen.quote} </p> : <p> Unfortunately, this queen is too basic to have a favourite quote... </p> }
-            {queen.missCongeniality? <p> 💐 Miss Congeniality</p>:''}
-            {queen.winner? <p>🏆Winner </p>:''}
+            {queen.quote !== `""` ? <p> <span className='span2'>Favourite quote: </span> {queen.quote} </p> : <p> Unfortunately, this queen is too basic to have a favourite quote... </p> }
+            {queen.missCongeniality? <p className='span2'> 💐 Miss Congeniality</p>:''}
+            {queen.winner? <p className='span2'>🏆Winner </p>:''}
             </div>
         </div>   )
 
