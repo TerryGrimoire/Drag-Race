68393a5f61315ce645156e930143542978647b35
Terry Grimoire <terrt.grimoire@gmail.com>
2022-05-22 17:24:21 +0200

    adding marie soleil content and design

--- src/Components/Sections.jsx
+++ src/Components/Sections.jsx
@@ -9,7 +9,7 @@ function Sections({ el }) {
     <main className='sections_main'>
         <section className='welcome_sections' onMouseOver={()=> setMouseOver(true)} onMouseLeave={()=>setMouseOver(false)} >
         <img className='welcome_rupaul_img' src={el.image} alt={el.title} />
-        <h3 className='section_title3'> {el.title} </h3>
+        <h3 className='sections_title3'> {el.title} </h3>
         {mouseOver? <div className='learn_more'> <p> Learn More about {el.title} </p> </div> : '' }
         </section>
         </main>
---
+++ src/data/marieso.js
@@ -0,0 +1,25 @@
+import mariesol from '../images/marieso.gif';
+import mariesolbcn from '../images/mariesobcn.gif';
+import mariesolrun from '../images/mariesorun2.gif';
+const marieso = [
+    {
+        id: 1,
+        title : 'Pride Reunion 2021',
+        image: mariesolrun,
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque reprehenderit atque esse laboriosam sapiente velit ex qui minus cum, veniam molestiae voluptatibus ab harum odio incidunt tempore facilis illum consequuntur eum perspiciatis, modi magnam culpa! Ad eveniet in mollitia quas deserunt aperiam obcaecati voluptates porro, tenetur, est dicta harum fugiat eum rem distinctio iure quis iste aut aspernatur numquam iusto autem? Cum laborum alias accusantium magni, iste culpa labore, tempora ratione quaerat, aperiam quod aliquam. Natus corrupti, et repellendus molestiae placeat, animi odit expedita est vero illo veniam! Molestiae facere alias magni praesentium debitis corporis labore magnam delectus odio libero excepturi sapiente, cum porro dolorum. Nemo nisi alias eligendi consequatur eos id ea officia quas non eius reprehenderit dolorem quam perspiciatis, accusamus quia in sed earum culpa vel! Ducimus hic unde molestiae, velit magni voluptates architecto, necessitatibus autem iusto nostrum nihil omnis rem nam, eos doloremque voluptate eius laudantium ratione!'
+    },
+    {
+        id: 2,
+        title : 'Pride Barcelona 2021',
+        image: mariesolbcn,
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque reprehenderit atque esse laboriosam sapiente velit ex qui minus cum, veniam molestiae voluptatibus ab harum odio incidunt tempore facilis illum consequuntur eum perspiciatis, modi magnam culpa! Ad eveniet in mollitia quas deserunt aperiam obcaecati voluptates porro, tenetur, est dicta harum fugiat eum rem distinctio iure quis iste aut aspernatur numquam iusto autem? Cum laborum alias accusantium magni, iste culpa labore, tempora ratione quaerat, aperiam quod aliquam. Natus corrupti, et repellendus molestiae placeat, animi odit expedita est vero illo veniam! Molestiae facere alias magni praesentium debitis corporis labore magnam delectus odio libero excepturi sapiente, cum porro dolorum. Nemo nisi alias eligendi consequatur eos id ea officia quas non eius reprehenderit dolorem quam perspiciatis, accusamus quia in sed earum culpa vel! Ducimus hic unde molestiae, velit magni voluptates architecto, necessitatibus autem iusto nostrum nihil omnis rem nam, eos doloremque voluptate eius laudantium ratione!'
+    },
+    {
+        id: 3,
+        title : 'Euro Pride Vienna 2019',
+        image: mariesol,
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque reprehenderit atque esse laboriosam sapiente velit ex qui minus cum, veniam molestiae voluptatibus ab harum odio incidunt tempore facilis illum consequuntur eum perspiciatis, modi magnam culpa! Ad eveniet in mollitia quas deserunt aperiam obcaecati voluptates porro, tenetur, est dicta harum fugiat eum rem distinctio iure quis iste aut aspernatur numquam iusto autem? Cum laborum alias accusantium magni, iste culpa labore, tempora ratione quaerat, aperiam quod aliquam. Natus corrupti, et repellendus molestiae placeat, animi odit expedita est vero illo veniam! Molestiae facere alias magni praesentium debitis corporis labore magnam delectus odio libero excepturi sapiente, cum porro dolorum. Nemo nisi alias eligendi consequatur eos id ea officia quas non eius reprehenderit dolorem quam perspiciatis, accusamus quia in sed earum culpa vel! Ducimus hic unde molestiae, velit magni voluptates architecto, necessitatibus autem iusto nostrum nihil omnis rem nam, eos doloremque voluptate eius laudantium ratione!'
+    },
+
+]
+export default marieso;
